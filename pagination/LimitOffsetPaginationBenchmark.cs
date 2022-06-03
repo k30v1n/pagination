df93df9df31551526ee3a6843ee9d7cb48f16bfc
Kelvin Ferreira <10376375+k30v1n@users.noreply.github.com>
2022-06-02 23:48:24 -0400

    adding limit offset pagination benchmark

---
+++ pagination/LimitOffsetPaginationBenchmark.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore;
+using pagination.Data;
+
+namespace pagination
+{
+    public class LimitOffsetPaginationBenchmark: IDisposable
+    {
+        const int ITEMS_PER_PAGE = 100;
+        const int TOTAL_PAGES_TO_NAVIGATE = 50;
+
+        private readonly DataDbContext _dbContext;
+        public LimitOffsetPaginationBenchmark() => _dbContext = new DataDbContext();
+        public void Dispose() => _dbContext.Dispose();
+
+
+        public void RunLimitOffsetPagination(string table)
+        {
+            var offset = 0;
+
+            for (int i = 0; i < TOTAL_PAGES_TO_NAVIGATE; i++)
+            {
+                _dbContext.FirstExampleData
+                    .FromSqlRaw($"SELECT * FROM pagination.{table} order by FirstName LIMIT {offset}, {ITEMS_PER_PAGE}")
+                    .ToList();
+
+                offset += ITEMS_PER_PAGE;
+            }
+        }
+
+        [Benchmark]
+        public void RunLimitOffsetPagination_Over_1k() => RunLimitOffsetPagination("1_FirstExample");
+
+        [Benchmark]
+        public void RunLimitOffsetPagination_Over_10k() => RunLimitOffsetPagination("2_SecondExample");
+
+        //[Benchmark]
+        //public void RunLimitOffsetPagination_Over_100k() => RunLimitOffsetPagination("3_ThirdExample");
+
+        //[Benchmark]
+        //public void RunLimitOffsetPagination_Over_1million() => RunLimitOffsetPagination("4_ForthExample");
+    }
+}
--- pagination/Program.cs
+++ pagination/Program.cs
@@ -1,20 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using Microsoft.EntityFrameworkCore;
 using pagination;
 using pagination.Data;
 using pagination.Seeds;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to pagination benchmarking!");
 
-var db = new DataDbContext();
+using (var db = new DataDbContext())
+{ 
+    Console.WriteLine("Migrating DB...");
+    db.Database.Migrate();
+    Console.WriteLine("DB migrated");
 
-Console.WriteLine("Migrating DB...");
-db.Database.Migrate();
-Console.WriteLine("DB migrated");
-
-
-{
     var seedGeneration = new SeedDataGeneration();
     seedGeneration.GenerateSeedData(db);
 }
+
+Console.WriteLine("Running benchmarks...");
+
+var benchmark = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly);
+#if DEBUG
+var summary = benchmark.Run(args, new DebugInProcessConfig());
+#else
+var summary = benchmark.Run(args, DefaultConfig.Instance);
+#endif
 
 Console.WriteLine("Finished");
